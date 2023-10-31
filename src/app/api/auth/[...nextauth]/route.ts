69eaca82edd6aa8474b25f7b0c0dada07ec69c44
ebonian <tanadon.santisan@gmail.com>
2023-10-31 21:26:42 +0700

    feat: nextauth google login route

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions: AuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        }),
+    ],
+    callbacks: {
+        signIn: async ({ user, account, profile }) => {
+            if (
+                account?.provider === 'google' &&
+                profile?.email?.endsWith('21@student.chula.ac.th')
+            ) {
+                return true;
+            }
+            return '/register';
+        },
+    },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
