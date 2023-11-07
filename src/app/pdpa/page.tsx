66672c4923cb410a88a840dc63b6ee6c4af74f84
tntons <saharat.nuamchit@gmail.com>
2023-11-07 17:53:02 +0700

    feat: pdpa

--- src/app/pdpa/page.tsx
+++ src/app/pdpa/page.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/header";
+import Link from 'next/link';
 
 
 export default function RegisterPage() {
@@ -6,10 +7,18 @@ export default function RegisterPage() {
         <div className="min-h-screen bg-[#F998B926]">
             <Header/>
             <div className="flex flex-col items-center">
-                <h1 className="text-[50px] mt-[65px]">นโยบายการใช้ข้อมูลส่วนบุคคล</h1>
+                <h1 className="text-[50px] mt-[10px] text-[25px] font-bold">นโยบายการใช้ข้อมูลส่วนบุคคล</h1>
                 <div>
-                    <h2 className="text-[24px] mt-[20px]">นโยบายการใช้ข้อมูลส่วนบุคคล</h2>
-                    <button className="w-[350px] h-[48px] bg-[#941214] text-white mt-[350px] rounded-[32px] shadow-2xl">ลงทะเบียน</button>
+                    <div className="w-[340px] h-[524px] bg-white flex justify-center rounded-[10px] mt-[10px]"> 
+                        <h2 className="text-[24px] mt-[20px] text-[16px]">นโยบายการใช้ข้อมูลส่วนบุคคล</h2>
+                    </div>
+                    <div className="flex flex-row">
+                        <input type="checkbox" className="form-checkbox h-5 w-5 text-blue-600" checked />
+                        <label className="ml-2 text-gray-700">ฉันยอมรับเงื่อนไขการสมัครของผู้จัดงาน</label>
+                    </div>
+                    <Link href="/pdpa" passHref>
+                        <button className="w-[350px] h-[48px] bg-[#941214] text-white rounded-[32px] shadow-2xl">รับทราบและยินยอม</button>
+                    </Link>
                 </div>
             </div>
         </div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/header";
+import Link from 'next/link';
 
 
 export default function RegisterPage() {
@@ -8,7 +9,9 @@ export default function RegisterPage() {
             <div className="flex flex-col items-center">
                 <h1 className="text-[50px] mt-[65px]">INTANIA RUN 2024</h1>
                 <h2 className="text-[24px] mt-[20px]">XX NOVEMBER 2023</h2>
-                <button className="w-[350px] h-[48px] bg-[#941214] text-white mt-[350px] rounded-[32px] shadow-2xl">ลงทะเบียน</button>
+                <Link href="/pdpa" passHref>
+                    <button className="w-[350px] h-[48px] bg-[#941214] text-white mt-[350px] rounded-[32px] shadow-2xl">ลงทะเบียน</button>
+                </Link>
             </div>
         </div>
     );
