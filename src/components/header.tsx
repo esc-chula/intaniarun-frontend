8fa0cd918bd971f48055b067a1cac7ae086f8b8a
ebonian <tanadon.santisan@gmail.com>
2023-11-11 16:20:14 +0700

    chore: status bar path logic

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,17 @@
+'use client';
+
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+
+const SHOW_STATUS_PATH = ['/register/info'];
 
 export default function Header() {
+    const pathname = usePathname();
+
+    const showStatusBar = SHOW_STATUS_PATH.includes(pathname);
+
     return (
-        <header className='fixed left-0 right-0 top-0 z-50 h-20 select-none bg-primary-400'>
+        <header className='fixed left-0 right-0 top-0 z-50 select-none bg-primary-400'>
             <div className='flex h-20 flex-col items-center  justify-center rounded-bl-[64px] bg-white py-1'>
                 <div className='relative h-full w-full'>
                     <Image
@@ -13,6 +22,7 @@ export default function Header() {
                     />
                 </div>
             </div>
+            {showStatusBar ? <></> : null}
         </header>
     );
 }
