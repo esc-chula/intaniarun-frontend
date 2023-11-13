cbc2fd685a6f58084fcbf31e711dc5e4ee81d385
ebonian <tanadon.santisan@gmail.com>
2023-11-13 17:12:20 +0700

    feat: landing coming soon banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Image from 'next/image';
+
 export default function Home() {
-    return <main>intania run 2024</main>;
+    return (
+        <main className='min-h-apple relative w-full'>
+            <div className='absolute z-20 grid h-full w-full place-content-center'>
+                <Image
+                    src='/landing/1.jpg'
+                    fill
+                    alt='banner'
+                    className='object-contain'
+                />
+            </div>
+            <div className='absolute z-10 h-full w-full backdrop-blur-lg'></div>
+            <Image
+                src='/landing/1.jpg'
+                fill
+                alt='background_image'
+                className='absolute z-0 h-full w-full'
+            />
+        </main>
+    );
 }
