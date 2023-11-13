f7b429167de271bdbaa712f291fb127345bdcbbf
ebonian <tanadon.santisan@gmail.com>
2023-11-13 20:50:44 +0700

    feat: new landing image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <main className='min-h-apple relative w-full'>
             <div className='absolute z-20 grid h-full w-full place-content-center'>
                 <Image
-                    src='/landing/1.jpg'
+                    src='/landing/1.png'
                     fill
                     alt='banner'
                     className='object-contain'
@@ -13,7 +13,7 @@ export default function Home() {
             </div>
             <div className='absolute z-10 h-full w-full backdrop-blur-lg'></div>
             <Image
-                src='/landing/1.jpg'
+                src='/landing/1.png'
                 fill
                 alt='background_image'
                 className='absolute z-0 h-full w-full'
