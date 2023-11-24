8ceaa4d21b20d7a9bd0af63cc6f89e5fdbb7d466
unknown <tanadon.santisan@gmail.com>
2023-11-24 19:43:51 +0700

    feat: enable public

--- src/app/register/type/page.tsx
+++ src/app/register/type/page.tsx
@@ -17,12 +17,11 @@ export default function StatusPage() {
     if (type === 'vip') {
         return (
             <div className='w-full space-y-10'>
-                {/* TODO: change to ภายนอกวันที่ 25 */}
                 <div className='space-y-2'>
-                    <h1 className='text-2xl font-bold'>ประเภทผู้สมัครภายใน</h1>
-                    <p className='text-sm text-slate-600'>
+                    <h1 className='text-2xl font-bold'>ประเภทผู้สมัคร</h1>
+                    {/* <p className='text-sm text-slate-600'>
                         เปิดให้ประชาชนทั่วไปสมัครในวันที่ 25 - 27 พฤศจิกายน 2566
-                    </p>
+                    </p> */}
                 </div>
                 <div className='flex w-full flex-col items-center justify-center space-y-10'>
                     <InfoCard
@@ -46,10 +45,10 @@ export default function StatusPage() {
         <div className='w-full space-y-10'>
             {/* TODO: change to ภายนอกวันที่ 25 */}
             <div className='space-y-2'>
-                <h1 className='text-2xl font-bold'>ประเภทผู้สมัครภายใน</h1>
-                <p className='text-sm text-slate-600'>
-                    เปิดให้ประชาชนทั่วไปสมัครในวันที่ 25 - 27 พฤศจิกายน 2566
-                </p>
+                <h1 className='text-2xl font-bold'>ประเภทผู้สมัคร</h1>
+                {/* <p className='text-sm text-slate-600'>
+                        เปิดให้ประชาชนทั่วไปสมัครในวันที่ 25 - 27 พฤศจิกายน 2566
+                    </p> */}
             </div>
             <div className='flex w-full flex-col items-center justify-center space-y-10'>
                 {registerBody.length <= 1 && (
@@ -66,7 +65,6 @@ export default function StatusPage() {
                         }}
                     />
                 )}
-
                 <InfoCard
                     title='นิสิตเก่าวิศวฯ จุฬาฯ'
                     subTitle='คณะวิศวกรรมศาสตร์'
@@ -80,7 +78,8 @@ export default function StatusPage() {
                         router.push('/register/info');
                     }}
                 />
-                {/* <InfoCard
+                {/* TODO: uncomment on 25 nov */}
+                <InfoCard
                     title='ประชาคมจุฬาฯ'
                     subTitle='นิสิตและนิสิตเก่าต่างคณะ,
                     คณาจารย์ บุคลากร, นักเรียนโรงเรียนสาธิตจุฬาฯ'
@@ -93,9 +92,8 @@ export default function StatusPage() {
                         );
                         router.push('/register/info');
                     }}
-                /> */}
-                {/* TODO: uncomment on 25 nov */}
-                {/* <InfoCard
+                />
+                <InfoCard
                     title='ประชาชนทั่วไป'
                     subTitle='บุคคลทั่วไปและครอบครัว'
                     price='700 บาท'
@@ -107,7 +105,7 @@ export default function StatusPage() {
                         );
                         router.push('/register/info');
                     }}
-                /> */}
+                />
                 {registerBody[0].type === 'ALUMNI' &&
                 registerBody.length > 1 ? (
                     <InfoCard
