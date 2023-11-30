cf014fc6824f824d5628ef4d9ae472d0960d0de8
unknown <tanadon.santisan@gmail.com>
2023-11-30 13:07:41 +0700

    feat: update registration date

--- src/configs/register.ts
+++ src/configs/register.ts
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
-export const OPEN_DATE = moment('2023-11-16');
-export const END_DATE = moment('2023-11-28T01:00');
+export const OPEN_DATE = moment('2023-12-03');
+export const END_DATE = moment('2023-12-04T01:00');
